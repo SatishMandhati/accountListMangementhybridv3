253821882e8f4104a100b1b4a63f513305807185
SatishMandhati <34026216+SatishMandhati@users.noreply.github.com>
2021-02-26 06:28:38 +0530

    updated geolocation code

--- src/js/viewModels/incidents.js
+++ src/js/viewModels/incidents.js
@@ -543,7 +543,7 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
                 var storename = storeObj.name;
                 // alert(storename);
 
-                navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+             /*   navigator.geolocation.getCurrentPosition(onSuccess, onError, {
                     timeout: 30000
                 });
 
@@ -552,23 +552,23 @@ define(['knockout', 'appController', 'ojs/ojmodule-element-utils', 'accUtils', '
                     var lang = position.coords.longitude;
 
                     //Google Maps
-                    var myLatlng = new google.maps.LatLng(lat, lang);
-                    var mapOptions = {
+                    //var myLatlng = new google.maps.LatLng(lat, lang);
+                /*    var mapOptions = {
                         zoom: 4,
                         center: myLatlng
                     }
-                    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+                   /* var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
                     var marker = new google.maps.Marker({
                         position: myLatlng,
                         map: map
-                    });
-                }
+                    });*/
+                //}
 
-                function onError(error) {
+               /* function onError(error) {
                     alert('code: ' + error.code + '\n' +
                         'message: ' + error.message + '\n');
                 }
-                google.maps.event.addDomListener(window, 'load', onSuccess);
+                //google.maps.event.addDomListener(window, 'load', onSuccess);*/
 
 
             };
